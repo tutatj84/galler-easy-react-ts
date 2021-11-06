6ec29c82b3e05edf3f4fb8558cafceaa8b33b105
tienna <tiennahe130396@fpt.edu.vn>
2021-11-06 16:54:33 +0700

    [config] get searching api

---
+++ src/components/ImagesContainer.tsx
@@ -0,0 +1,20 @@
+// react
+import { FunctionComponent } from "react"
+//lib
+import styled from 'styled-components'
+
+interface ImagesContainerProps {
+
+}
+
+const ImagesContainer: FunctionComponent<ImagesContainerProps> = () => {
+	return (
+		<div className="images-container">
+			images
+		</div>
+	);
+}
+
+export default styled(ImagesContainer) `
+	
+`
--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -1,15 +1,54 @@
-import { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react"
+import styled from "styled-components";
+import ImagesContainer from "../components/ImagesContainer";
+import * as Constants from '../Constant'
 
 interface SearchProps {
-
+	className?: string
 }
 
-const Search: FunctionComponent<SearchProps> = () => {
+const startSearching = async (keyword: string, limit: number, offset: number) => {
+	const searchUrl = `${Constants.BASE_URL}?\
+											api_key=${Constants.API_KEY}\
+											&q=${keyword}\
+											&limit=${limit + offset}\
+											&offset=${offset}\
+										`
+	const response = await fetch(searchUrl)
+	const data = await response.json()
+	return data
+}
+
+const Search: FunctionComponent<SearchProps> = (props) => {
+	const [txtSearch, setTxtSearch] = useState('')
+
+	const { className } = props
+
+	// config for search
+	const limitSearch = 8
+	const offsetSearch = 0
+
+	const handleSubmit = async (e: React.FormEvent) => {
+		e.preventDefault();
+		const data = await startSearching(txtSearch, limitSearch, offsetSearch)
+		console.log(data)
+		
+	}
+
 	return (
-		<div className="Search">
-			Search Screen
+		<div className={className}>
+			<form onSubmit={async (e) => await handleSubmit(e)}>
+				<input
+					type="text"
+					placeholder='Enter a keyword here!'
+					value={txtSearch}
+					onChange={(e) => setTxtSearch(e.target.value)} />
+			</form>
+			<ImagesContainer />
 		</div>
 	);
 }
 
-export default Search;
+export default styled(Search) `
+	
+`
