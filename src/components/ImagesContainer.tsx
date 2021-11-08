5708230fc0273058962dbb703d1fc86d254d7777
tienna <tiennahe130396@fpt.edu.vn>
2021-11-08 17:22:24 +0700

    [add] lazy load for Image

--- src/components/ImagesContainer.tsx
+++ src/components/ImagesContainer.tsx
@@ -25,8 +25,19 @@ const ImageBox: FunctionComponent<ImageBoxProps> = (props) => {
 	const { imageUrl } = props
 
 	const [isFav, setIsFav] = useState(false)
+	const [mainGifUrl, setMainGifUrl] = useState('')
 	const isFavRef = useRef<boolean>()
 	const { setFavImages } = useContext(FavPicsContext)
+
+	const loadingGifUrl = 'https://img.pikbest.com/58pic/35/39/61/62K58PICb88i68HEwVnm5_PIC2018.gif!w340'
+
+	useEffect(() => {
+		const imageHelper = new Image()
+		imageHelper.src = imageUrl
+		imageHelper.onload = () => {
+			setMainGifUrl(imageUrl)
+		}
+	}, [])
 
 	const toggleFavStatus = (key: string) => {
 		isFavRef.current = !isFav
@@ -41,7 +52,7 @@ const ImageBox: FunctionComponent<ImageBoxProps> = (props) => {
 	}
 
 	const imageStyle = {
-		backgroundImage: `url(${imageUrl})`,
+		backgroundImage: `url(${mainGifUrl || loadingGifUrl})`,
 	}
 
 	useEffect(() => {
@@ -84,7 +95,7 @@ export default styled(ImagesContainer)`
 	flex-wrap: wrap;
 	gap: 1rem;
 	padding: 2rem;
-
+	justify-content: center;
 
 	.image-box {
 		height: 13rem;
